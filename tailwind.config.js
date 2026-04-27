7741d9a5cbf69e2ba1288e676dcf7a23d53a09a9
Thomas <thomas.galocha@hotmail.fr>
2026-04-27 17:30:36 +0200

    migrate to Astro for static site generation and SEO

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue,js}'],
+  content: ['./src/**/*.{astro,js,ts}'],
   theme: {
     extend: {
       colors: {
